fd7a7d058d53fc6ec6872592a0e0214a4ec5600b
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-03-27 10:21:43 +0200

    Implement toggle functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// phew… not a lot going on here. Please add some code!
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerText = document.querySelector('[data-js="answer-question"]');
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+answerButton.addEventListener("click", () => {
+  answerText.classList.toggle("card__answer--active");
+  const answerButtonText = answerButton.textContent.trim();
+  answerButton.textContent =
+    answerButtonText === "Show answer" ? "Hide answer" : "Show answer";
+});
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
